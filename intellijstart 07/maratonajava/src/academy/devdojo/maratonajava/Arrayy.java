624093abfd2ca6386c7410bf6bd5a686dc12cb98
Fernando Arimori <fernando.c.arimori@gmail.com>
2022-06-13 11:34:16 -0300

    Exercisejava basic

---
+++ intellijstart 07/maratonajava/src/academy/devdojo/maratonajava/Arrayy.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava;
+
+public class Arrayy {
+    public static void main(String[] args) {
+        String[] nome = new String[3];
+        nome[0] ="456";
+        nome[1] ="4563";
+        nome[2] ="41";
+
+        for(int i =0; i<nome.length; i++){
+            System.out.println(nome[i]);
+        }
+    }
+}
